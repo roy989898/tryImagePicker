542686f257dc7965d62e1f2a396223749412ed91
roy989898 <roy989898@gmail.com>
2016-06-21 11:05:53 +0800

    feat:can choose image.cap image,show image

---
+++ app/src/main/java/pom/poly/com/tryimagepricker/MainActivity.java
@@ -0,0 +1,49 @@
+package pom.poly.com.tryimagepricker;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.soundcloud.android.crop.Crop;
+import com.squareup.picasso.Picasso;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView im;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        im = (ImageView) findViewById(R.id.im);
+        Crop.pickImage(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (requestCode == Crop.REQUEST_PICK && resultCode == RESULT_OK) {
+            beginCrop(result.getData());
+        } else if (requestCode == Crop.REQUEST_CROP) {
+            handleCrop(resultCode, result);
+        }
+    }
+
+    private void beginCrop(Uri source) {
+        Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+        Crop.of(source, destination).asSquare().start(this);
+    }
+
+    private void handleCrop(int resultCode, Intent result) {
+        if (resultCode == RESULT_OK) {
+//            resultView.setImageURI(Crop.getOutput(result));
+            Picasso.with(this).load(Crop.getOutput(result)).into(im);
+        } else if (resultCode == Crop.RESULT_ERROR) {
+            Toast.makeText(this, Crop.getError(result).getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
